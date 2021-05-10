cc68af09dbf7aa56ac3c732569dfb0ed0a2ef6e0
Jackson Graff <jacksongraff@hotmail.com>
2021-05-10 12:34:46 -0300

    inicio do projeto e css global

---
+++ 02-dtmoney/src/App.tsx
@@ -0,0 +1,14 @@
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <div className="App">
+      <head>
+        <GlobalStyle />
+      </head>
+      <body>
+        <h1>Hello World</h1>
+      </body>
+    </div>
+  );
+}
