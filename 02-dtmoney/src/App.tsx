55a6630671283187fc53eb890cba166ed35d8928
Jackson Graff <jacksongraff@hotmail.com>
2021-05-11 18:45:29 -0300

    created header

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,14 +1,11 @@
+import { Header } from "./components/Header"
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <head>
-        <GlobalStyle />
-      </head>
-      <body>
-        <h1>Hello World</h1>
-      </body>
-    </div>
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
   );
 }
