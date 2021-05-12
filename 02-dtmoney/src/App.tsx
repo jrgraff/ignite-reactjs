e4ee05e51088a72d0eb0fe1b566bb38f4f2bd92a
Jackson Graff <jacksongraff@hotmail.com>
2021-05-12 14:04:01 -0300

    new transaction

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header"
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewModalTransactionOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewModalTransactionOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewModalTransactionOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <Dashboard />
       <GlobalStyle />
     </>
